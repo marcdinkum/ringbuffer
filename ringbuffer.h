c44665ae321617aa7f16c98950abf743283a96be
Wouter Ensink <wouter@mbp-van-wouter.home>
2019-10-13 19:25:24 +0200

    made the ringbuffer class generic(but only with float, double and long double) and updated the code style to more modern C++14

--- ringbuffer.h
+++ ringbuffer.h
@@ -1,32 +1,44 @@
-/*
- * ringbuffer.h
- */
+
+//ringbuffer.h
 
 #include <atomic>
 #include <string>
 
+
+template<typename FloatType>
 class RingBuffer
 {
 public:
-  RingBuffer(unsigned long size,std::string name);
-  ~RingBuffer();
-  unsigned long push(float *data,unsigned long n);
-  unsigned long pop(float *data,unsigned long n);
-  unsigned long items_available_for_write();
-  unsigned long items_available_for_read();
-  bool isLockFree();
-  void pushMayBlock(bool block);
-  void popMayBlock(bool block);
-  void setBlockingNap(unsigned long blockingNap);
-private:
-  unsigned long size;
-  float *buffer;
-  std::atomic<unsigned long> tail; // write pointer
-  std::atomic<unsigned long> head; // read pointer
-  unsigned long itemsize; // also depends on #channels
-  std::string name;
-  bool blockingPush;
-  bool blockingPop;
-  unsigned long blockingNap=500;
-}; // RingBuffer{}
 
+    using uint64 = unsigned long;
+
+    RingBuffer(uint64 size, const std::string& name);
+    ~RingBuffer();
+
+    auto push(FloatType* data, uint64 numSamples) -> uint64;
+    auto pop(FloatType* data, uint64 numSamples) -> uint64;
+    auto numItemsAvailableForWrite() const -> uint64;
+    auto numItemsAvailableForRead() const -> uint64;
+
+    auto isLockFree() const -> bool;
+    auto pushMayBlock(bool block) -> void;
+    auto popMayBlock(bool block) -> void;
+    auto setBlockingNap(uint64 blockingNap) -> void;
+
+private:
+
+    uint64 size;
+    FloatType* buffer;
+
+    std::atomic<uint64> tail; // write index
+    std::atomic<uint64> head; // read index
+
+    static constexpr uint64 itemSize { sizeof(FloatType) };
+
+    const std::string name;
+
+    bool blockingPush;
+    bool blockingPop;
+
+    uint64 blockingNap { 500 };
+};
